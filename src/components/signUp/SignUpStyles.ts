2c3878a867b348e521e94e5843650b446fb92085
Julio Gómez Campanario <gomcam.julio@icloud.com>
2020-06-27 14:01:17 +0200

    Login Margins
    Form style was removed and now LogIn margins and SignUp margins match
    
    This closes #38

--- src/components/signUp/SignUpStyles.ts
+++ src/components/signUp/SignUpStyles.ts
@@ -2,9 +2,6 @@ import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 
 export default makeStyles((theme: Theme) =>
   createStyles({
-    form: {
-      padding: '0px 0px 0px 0px'
-    },
     form_control: {
       width: '100%'
     },
